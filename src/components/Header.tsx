71387c415dcde7980d36b399253b38f92bcf3b7e
sikatanju <saadtanju@gmail.com>
2025-01-01 09:06:46 +0600

    Updated logo, and added navbar for large screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ const Header = () => {
     const [navOpen, setNavOpen] = useState<boolean>(true);
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,.6fr,1fr]">
                 <h1>
                     <a href="/" className="logo">
                         <img
-                            src="/images/st.svg"
+                            src="/images/logo.svg"
                             width={40}
                             height={40}
                             alt="Sika Tanju"
@@ -34,7 +34,6 @@ const Header = () => {
                     href="#"
                     className="btn btn-secondary max-md:hidden md:justify-self-end"
                 >
-                    {" "}
                     Contact Me
                 </a>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,63 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useRef } from "react";
+import { SyntheticEvent, useEffect, useRef } from "react";
 
 interface Props {
     navOpen: boolean | null;
 }
 
 const Navbar = ({ navOpen }: Props) => {
-    const lastActiveLink = useRef(null);
-    const activeBox = useRef(null);
+    const lastActiveLink = useRef<HTMLElement | null>(null);
+    const activeBox = useRef<HTMLDivElement | null>(null);
+
+    const initActiveBox = () => {
+        if (activeBox.current && lastActiveLink.current) {
+            const activeBoxEl = activeBox.current;
+            const lastActiveLinkEl = lastActiveLink.current;
+
+            activeBoxEl.style.top = `${lastActiveLinkEl.offsetTop}px`;
+            activeBoxEl.style.left = `${lastActiveLinkEl.offsetLeft}px`;
+            activeBoxEl.style.width = `${lastActiveLinkEl.offsetWidth}px`;
+            activeBoxEl.style.height = `${lastActiveLinkEl.offsetHeight}px`;
+        }
+    };
+
+    useEffect(() => {
+        const homeLink = document.querySelector(
+            ".nav-link.active"
+        ) as HTMLElement;
+        if (homeLink) {
+            lastActiveLink.current = homeLink;
+            initActiveBox();
+        }
+    }, []);
+    window.addEventListener("resize", initActiveBox);
+
+    const activeCurrentLink = (event: SyntheticEvent) => {
+        if (lastActiveLink.current) {
+            lastActiveLink.current.classList.remove("active");
+        }
+        if (event.target instanceof HTMLElement) {
+            event.target.classList.add("active");
+            lastActiveLink.current = event.target;
+            initActiveBox();
+        }
+    };
+
     const navItems = [
-        {
-            label: "Home",
-            link: "#home",
-            className: "nav-link active",
-            ref: lastActiveLink,
-        },
-        {
-            label: "About",
-            link: "#about",
-            className: "nav-link",
-        },
-        {
-            label: "Work",
-            link: "#work",
-            className: "nav-link",
-        },
-        {
-            label: "Reviews",
-            link: "#reviews",
-            className: "nav-link",
-        },
-        {
-            label: "Contact",
-            link: "#contact",
-            className: "nav-link md:hidden",
-        },
+        { label: "Home", link: "#home", className: "nav-link active" },
+        { label: "About", link: "#about", className: "nav-link" },
+        { label: "Work", link: "#work", className: "nav-link" },
+        { label: "Reviews", link: "#reviews", className: "nav-link" },
+        { label: "Contact", link: "#contact", className: "nav-link md:hidden" },
     ];
+
     return (
-        <nav className={"navbar " + (navOpen ? "active" : "")}>
-            {navItems.map(({ label, link, className, ref }, key) => (
+        <nav className={`navbar ${navOpen ? "active" : ""}`}>
+            {navItems.map(({ label, link, className }, key) => (
                 <a
                     href={link}
                     className={className}
-                    ref={ref}
-                    onClick={() => {}}
+                    onClick={activeCurrentLink}
                     key={key}
                 >
                     {label}
