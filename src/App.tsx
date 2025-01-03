e9605042fe6d154f75eb69ca9e9d1f7dfaaae9f6
sikatanju <saadtanju@gmail.com>
2025-01-03 07:41:38 +0600

    Updated stylesheet to fetch google icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import About from "./components/About";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Skill from "./components/Skill";
 
 function App() {
     return (
         <>
             <Header />
+            <Hero />
+            <About />
+            <Skill />
         </>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import Navbar from "./Navbar";
 
 const Header = () => {
-    const [navOpen, setNavOpen] = useState<boolean>(true);
+    const [navOpen, setNavOpen] = useState(false);
+
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,.6fr,1fr]">
+            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
                 <h1>
                     <a href="/" className="logo">
                         <img
@@ -17,12 +17,13 @@ const Header = () => {
                         />
                     </a>
                 </h1>
-                <div className="relative md:justfiy-self-center">
+
+                <div className="relative md:justify-self-center">
                     <button
                         className="menu-btn md:hidden"
-                        onClick={() => setNavOpen(!navOpen)}
+                        onClick={() => setNavOpen((prev) => !prev)}
                     >
-                        <span className="material-symbols-outlined">
+                        <span className="material-symbols-rounded">
                             {navOpen ? "close" : "menu"}
                         </span>
                     </button>
@@ -31,7 +32,7 @@ const Header = () => {
                 </div>
 
                 <a
-                    href="#"
+                    href="#contact"
                     className="btn btn-secondary max-md:hidden md:justify-self-end"
                 >
                     Contact Me
