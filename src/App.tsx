d44e3c12d7cbab0dcb62afc95201ebe7623fdcb2
sikatanju <saadtanju@gmail.com>
2025-01-04 09:58:10 +0600

    Updated Navbar, added Contact, skill and projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
             <Hero />
             <About />
             <Skill />
+            <Work />
+            <Contact />
         </>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,8 +46,8 @@ const Navbar = ({ navOpen }: Props) => {
     const navItems = [
         { label: "Home", link: "#home", className: "nav-link active" },
         { label: "About", link: "#about", className: "nav-link" },
-        { label: "Work", link: "#work", className: "nav-link" },
-        { label: "Reviews", link: "#reviews", className: "nav-link" },
+        { label: "Skill", link: "#skills", className: "nav-link" },
+        { label: "Projects", link: "#projects", className: "nav-link" },
         { label: "Contact", link: "#contact", className: "nav-link md:hidden" },
     ];
 
